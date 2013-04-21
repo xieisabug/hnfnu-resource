d627c66873a59288c02c35fee97fb7de72b285ac
xjyaikj@gmail.com <xjyaikj@gmail.com@8bd0d043-5b41-ff9d-f009-2dea3a7eb2be>
2013-04-21 08:57:19 +0000

    index。html初步完成，但是出了有奇葩的错误。。。

--- hnfnuzyw/WebRoot/App/index.js
+++ hnfnuzyw/WebRoot/App/index.js
@@ -1,14 +1,58 @@
-var tab,layout,accordion;
-
+var tab, layout, accordion;
 $(function () {
+
+    function f_heightChanged(options) {
+        if (tab)
+            tab.addHeight(options.diff);
+        if (accordion && options.middleHeight - 24 > 0)
+            accordion.setHeight(options.middleHeight - 24);
+    }
+
     layout = $("#main_body").ligerLayout({
         height:'100%',
         heightDiff:-3,
-        leftWidth:200
+        leftWidth:200,
+        onHeightChanged:f_heightChanged
     });
     var bodyHeight = $(".l-layout-center:first").height();
-    accordion = $("#main_menu").ligerAccordion({
-        height:bodyHeight - 25
-    });
+
     tab = $("#mainframe").ligerTab();
+    var main_menu = $("#main_menu");
+
+    $.get('welcome.html', function (data) {
+            $('#welcome').html(data)
+        }
+    )
+
+    //菜单初始化
+    $("ul.menulist li").live("mouseover",function () {
+        $(this).addClass("over");
+    }).live("mouseout", function () {
+        $(this).removeClass("over");
+    });
+
+
+    /* 数据库获得菜单*/
+    $.getJSON('./Json/menu.json', function (menus) {
+            $(menus).each(function (i, menu) {
+                var item = $('<div title="' + menu.name + '"> <ul class="menuList"></ul></div>');
+                $(menu.children).each(function (i, submenu) {
+                    var subitem = $('<li><img/><span></span><div class="menuitem-l"></div><div class="menuitem-r"></div></li>');
+                    subitem.attr({
+                        url:submenu.url,
+                        menuno:submenu.id
+                    });
+                    $("img", subitem).attr("src", submenu.icon || submenu.menuicon);
+                    $("span", subitem).html(submenu.menuname || submenu.name || submenu.text);
+                    $("ul:first", item).append(subitem);
+                });
+                main_menu.append(item);
+            });
+            accordion = $("#main_menu").ligerAccordion({
+                height:bodyHeight - 25
+            });
+        }
+    );
+
+
 });
