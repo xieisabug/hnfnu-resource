bfc34a8cc0f2cc0f7df7bb51dcc2ef72d35bb208
xjyaitf@gmail.com <xjyaitf@gmail.com@8bd0d043-5b41-ff9d-f009-2dea3a7eb2be>
2013-04-23 07:59:06 +0000

    功能管理各个功能搭建

--- hnfnuzyw/WebRoot/App/System/Function/FunctionManage.js
+++ hnfnuzyw/WebRoot/App/System/Function/FunctionManage.js
@@ -1,27 +1,45 @@
-function add_function(){
-    $.ligerDialog.alert('add');
+var functionGrid = null;
+function add_function() {
+    $.ligerDialog.open({
+        width:500,
+        height:500,
+        title:'新增功能',
+        url:'FunctionAdd.html',
+        buttons:[
+            {text:'提交',width:80,onclick:function(a,b,c){console.log(a);console.log(b);console.log(c);}}
+        ]
+    });
 }
-function edit_function(){
+function edit_function() {
     $.ligerDialog.alert('edit');
 }
-function delete_function(){
-    $.ligerDialog.confirm('确认删除？');
+function delete_function() {
+    var row = functionGrid.getSelected();
+    $.ligerDialog.confirm('确认删除' + row.name + '？', '删除功能', function (r) {
+        if (r) {
+            //todo 进行ajax操作，成功后在回调函数里删除选择的行
+            functionGrid.deleteSelectedRow();
+        }
+    });
 }
 $(function () {
-    $('#functionGrid').ligerGrid({
+    functionGrid = $('#functionGrid').ligerGrid({
         columns:[
-            { display: 'ID', name: 'id', align: 'left', width: 100 },
-            { display: '功能名称', name: 'name', width: 200 },
-            { display: '备注', name: 'remark', align: 'left', width: 400 }
+            { display:'ID', name:'id', align:'left', width:100 },
+            { display:'功能名称', name:'name', width:200 },
+            { display:'备注', name:'remark', align:'left', width:400 }
         ],
-        width:'90%',
-        height:'90%',
+        width:'99%',
+        height:'95%',
+        pageSize:30,
+        url:'../../../Json/FunctionData.json',
         toolbar:{
             items:[
-                {text:'新增功能',click:add_function,icon:'add'},
-                {text:'修改功能',click:edit_function,icon:'modify'},
-                {text:'删除功能',click:delete_function,icon:'delete'}
+                {text:'新增功能', click:add_function, icon:'add'},
+                {text:'修改功能', click:edit_function, icon:'modify'},
+                {text:'删除功能', click:delete_function, icon:'delete'}
             ]
         }
     });
+    $("#pageloading").hide();
 });
