d3febb030784479ac487bcf9c88ac5ab9eb848e7
xjyaikj@gmail.com <xjyaikj@gmail.com@8bd0d043-5b41-ff9d-f009-2dea3a7eb2be>
2013-04-23 05:27:13 +0000

    实现了菜单的管理界面，但是在ie下还有点点小问题

--- trunk/hnfnuzyw/WebRoot/App/System/Menu/MenuManage.js
+++ trunk/hnfnuzyw/WebRoot/App/System/Menu/MenuManage.js
@@ -1,14 +1,32 @@
 var grid = null;
 
-$(function(){
-        grid = $("menugrid").ligerGrid({
-            columns:[
-                {display:'菜单id',name:'id',align:'left',width:120},
-                {display:'菜单名字',name:'name',align:'left',minWidth:80},
-                {display:'菜单链接',name:'url',align:'left',minWidth:80},
-                {display:'功能列表',name:'functionIdList',align:'left',minWidth:100},
-                {display:'图标链接',name:'' }
+function itemclick(item) {
+    $.ligerDialog.alert(item.text);
+}
+
+$(function () {
+    grid = $("#menuGrid").ligerGrid({
+        columns:[
+            {display:'菜单id', name:'id', align:'left', width:120},
+            {display:'菜单名字', name:'name', align:'left', minWidth:80},
+            {display:'菜单链接', name:'url', align:'left', minWidth:120},
+            {display:'功能列表', name:'functionIdList', align:'left', minWidth:100},
+            {display:'图标链接', name:'icon', align:'left', minWidth:120},
+            {display:'上级菜单', name:'parentId', align:'left', minWidth:100}
+        ],
+        url:'../../../Json/MenuData.json',
+        height:'100%', width:'100%',
+        toolbar:{
+            items:[
+                {text:'增加', click:itemclick, icon:'add' },
+                { line:true },
+                {text:'删除', click:itemclick, icon:'delete'},
+                {line:true},
+                {text:'修改', click:itemclick, icon:'modify'},
+                {line:true},
+                {text:'刷新', click:itemclick, icon:'refresh'}
             ]
-        });
-    }
-);
+        }
+    });
+    //$("#pageloading").hide();
+});
