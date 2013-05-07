67bb531bb4d5059bed40b4650ac966b742df1f19
xjyaitf@gmail.com <xjyaitf@gmail.com@8bd0d043-5b41-ff9d-f009-2dea3a7eb2be>
2013-05-07 06:43:47 +0000

    完成修改功能的表单自动读取数据功能。 完成工具条限制功能。 主页添加加载数据遮罩。

--- hnfnuzyw/WebRoot/App/System/Function/FunctionManage.js
+++ hnfnuzyw/WebRoot/App/System/Function/FunctionManage.js
@@ -1,51 +1,74 @@
-var functionGrid = null;
-var addFunctionForm = null;
-var functionWin = null;
+var functionGrid = null;//功能表格
+var functionForm = null;//功能表单
+var functionWin = null;//功能窗口
 
+//增加功能的函数
 function add_function() {
-    if(!addFunctionForm){
+    if (!functionForm) {
         formInit();
     } else {
-        addFunctionForm[0].reset();
+        functionForm[0].reset();
     }
     functionWin = $.ligerDialog.open({
         width:400,
         height:200,
         title:'新增功能',
-        target:addFunctionForm,
+        target:functionForm,
         buttons:[
             {text:'提交', width:80, onclick:add_save},
             {text:'取消', width:80, onclick:add_cancel}
         ]
     });
 }
+//增加功能的保存按钮事件
 function add_save() {
-    var data = Form.parseJSON(addFunctionForm);
+    var data = Form.parseJSON(functionForm);
     //todo 需要发往服务器，返回成功后再添加到表格中
     functionGrid.addRow(data);
     functionWin.close();
 }
+//增加功能的取消按钮事件
 function add_cancel() {
     functionWin.close();
 }
+//修改功能的函数
 function edit_function() {
-    if(!addFunctionForm){
+    if (!functionForm) {
         formInit();
     }
-    Form.loadForm(addFunctionForm,functionGrid.getSelected());
-
+    if (!functionGrid.getSelected()) {
+        $.ligerDialog.warn('请选择您要修改的行.');
+        return;
+    }
+    Form.loadForm(functionForm, functionGrid.getSelected());
     functionWin = $.ligerDialog.open({
         width:400,
         height:200,
         title:'编辑功能',
-        target:addFunctionForm,
+        target:functionForm,
         buttons:[
-            {text:'提交', width:80, onclick:add_save},
-            {text:'取消', width:80, onclick:add_cancel}
+            {text:'提交', width:80, onclick:edit_save},
+            {text:'取消', width:80, onclick:edit_cancel}
         ]
     });
 }
+//修改功能的保存按钮事件
+function edit_save() {
+    var data = Form.parseJSON(functionForm);
+    //todo 需要发往服务器，返回成功后再修改到表格中
+    functionGrid.update(functionGrid.getSelected(), data);
+    functionWin.close();
+}
+//修改功能的取消按钮事件
+function edit_cancel() {
+    functionWin.close();
+}
+//删除功能的函数
 function delete_function() {
+    if (!functionGrid.getSelected()) {
+        $.ligerDialog.warn('请选择您要删除的行.');
+        return;
+    }
     var row = functionGrid.getSelected();
     $.ligerDialog.confirm('确认删除' + row.name + '？', '删除功能', function (r) {
         if (r) {
@@ -54,9 +77,10 @@ function delete_function() {
         }
     });
 }
-function formInit(){
-    addFunctionForm = $('<form></form>');
-    addFunctionForm.ligerForm({
+//初始化表单，生成form标签
+function formInit() {
+    functionForm = $('<form></form>');
+    functionForm.ligerForm({
         inputWidth:280,
         fields:[
             {
@@ -97,7 +121,19 @@ function formInit(){
         ]
     });
 }
+//初始化表格
 $(function () {
+    var toolbarItems = [
+        {text:'新增功能', click:add_function, icon:'add', key:'add'},
+        {text:'修改功能', click:edit_function, icon:'modify', key:'edit'},
+        {text:'删除功能', click:delete_function, icon:'delete', key:'delete'}
+    ];
+    //todo 以后这个ajaxToolbar要通过ajax取过来
+    var ajaxToolbar = [
+        {name:'add'},
+        {name:'delete'}
+    ];
+    toolbarItems = Toolbar.confirmToolbar(toolbarItems,ajaxToolbar);
     functionGrid = $('#functionGrid').ligerGrid({
         columns:[
             { display:'ID', name:'id', align:'left', width:100 },
@@ -105,15 +141,11 @@ $(function () {
             { display:'备注', name:'remark', align:'left', width:400 }
         ],
         width:'99%',
-        height:'95%',
+        height:'98%',
         pageSize:30,
         url:'../../../Json/FunctionData.json',
         toolbar:{
-            items:[
-                {text:'新增功能', click:add_function, icon:'add'},
-                {text:'修改功能', click:edit_function, icon:'modify'},
-                {text:'删除功能', click:delete_function, icon:'delete'}
-            ]
+            items:toolbarItems
         }
     });
     $("#pageloading").hide();
