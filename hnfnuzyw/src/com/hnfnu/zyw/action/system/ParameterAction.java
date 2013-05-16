529a1c264f59149d08dcb22c38d42163f79e90c9
xjyaitf@gmail.com <xjyaitf@gmail.com@8bd0d043-5b41-ff9d-f009-2dea3a7eb2be>
2013-05-16 13:28:22 +0000

    参数action和service的接口定义

---
+++ hnfnuzyw/src/com/hnfnu/zyw/action/system/ParameterAction.java
@@ -0,0 +1,77 @@
+package com.hnfnu.zyw.action.system;
+
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.Namespace;
+import org.apache.struts2.convention.annotation.ParentPackage;
+import org.apache.struts2.convention.annotation.Result;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+
+import com.hnfnu.zyw.dto.system.ParameterDto;
+import com.hnfnu.zyw.service.system.IParameterService;
+import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.ModelDriven;
+
+@Controller("parameterAction")
+@Scope("prototype")
+@ParentPackage("json-default")
+@Namespace("/system")
+public class ParameterAction extends ActionSupport implements ModelDriven<ParameterDto> {
+
+	private static final long serialVersionUID = 361910504650120895L;
+
+	@Autowired
+	@Qualifier("parameterService")
+	private IParameterService parameterService;
+	
+	private ParameterDto parameter = new ParameterDto();//获取页面提交参数
+	private boolean success;
+
+	@Action(value = "addParameter", results = { @Result(name = "success", type="json") })
+	public String add(){
+		success = parameterService.add(parameter);
+		return SUCCESS;
+	}
+	
+	@Action(value = "updateParameter", results = { @Result(name = "success", type="json") })
+	public String update(){
+		success = parameterService.update(parameter);
+		return SUCCESS;
+	}
+	
+	@Action(value = "loadParameter", results = { @Result(name = "success", type="json") })
+	public String load(){
+		parameter = parameterService.load(parameter);
+		return SUCCESS;
+	}
+	
+	@Action(value = "deleteParameter", results = { @Result(name = "success", type="json") })
+	public String delete(){
+		success = parameterService.delete(parameter);
+		return SUCCESS;
+	}
+
+	/*get set*/
+	public IParameterService getParameterService() {
+		return parameterService;
+	}
+
+	public void setParameterService(IParameterService parameterService) {
+		this.parameterService = parameterService;
+	}
+
+	public ParameterDto getModel() {
+		return parameter;
+	}
+
+	public ParameterDto getParameter() {
+		return parameter;
+	}
+
+	public boolean isSuccess() {
+		return success;
+	}
+	
+}
