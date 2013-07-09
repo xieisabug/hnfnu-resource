c8af48896cdeb580a18e5857e7f12c66591c888c
xjyaikj@gmail.com <xjyaikj@gmail.com@8bd0d043-5b41-ff9d-f009-2dea3a7eb2be>
2013-07-09 13:41:59 +0000

    课程管理前后台都写完了，但是添加课程的下拉框有点问题笨笨调不出

---
+++ hnfnuzyw/WebRoot/App/Resources/Course/CourseManage.js
@@ -0,0 +1,297 @@
+var courseGrid = null;
+var courseForm = null;
+var courseWin = null;
+// 按钮的click事件
+function update_course(item) {
+	$.ligerDialog.alert(item.text);
+}
+// 增加课程的函数
+function add_course() {
+
+	formInit();
+
+	courseWin = $.ligerDialog.open( {
+		width : 400,
+		height : 400,
+		title : "新增课程",
+		target : courseForm,
+		buttons : [ {
+			text : "提交",
+			width : 80,
+			onclick : add_save
+		}, {
+			text : "取消",
+			width : 80,
+			onclick : add_cancel
+		} ]
+	});
+}
+// 增加课程的保存按钮事件
+function add_save() {
+	// 把表单转化为数组
+	if (courseForm.valid()) {
+		var row_data = Form.parseJSON(courseForm);
+		console.log(row_data);
+		// 发往服务器，返回成功后再添加到表格中
+		$.ajax( {
+			url : '/hnfnuzyw/resources/addCourse.action',
+			data : row_data,
+			type : 'post',
+			success : function(data) {
+				console.log(data);
+				if (data.success) {
+					courseGrid.addRow(data.model);
+					$.ligerDialog.tip( {
+						title : '提示信息',
+						content : data.message
+					});
+					courseWin.close();
+				} else {
+					$.ligerDialog.error(data.message);
+				}
+			}
+		});
+	}
+}
+// 增加课程的取消按钮事件
+function add_cancel() {
+	courseWin.close();
+}
+
+// 删除课程的函数
+function delete_course() {
+	if (!courseGrid.getSelected()) {
+		$.ligerDialog.warn("请选择您要删除的行！");
+		return;
+	}
+	var row_data = courseGrid.getSelected();
+	$.ligerDialog.confirm('确认删除' + row_data.name + '?', '删除功能', function(r) {
+		if (r) {
+			// todo 进行ajax操作，成功后在回调函数里删除选择的行
+			$.ajax( {
+				url : '/hnfnuzyw/resources/deleteCourse.action',
+				data : row_data,
+				type : 'post',
+				success : function(data) {
+					if (data.success) {
+						$.ligerDialog.tip( {
+							title : '提示信息',
+							content : data.message
+						});
+						courseGrid.deleteSelectedRow();
+						courseWin.close();
+					} else {
+						$.ligerDialog.error(data.message);
+					}
+				}
+			})
+		}
+	});
+
+}
+
+// 修改课程的函数
+function edit_course() {
+	formInit();
+	if (!courseGrid.getSelected()) {
+		$.ligerDialog.warn("请选择您要修改的行！");
+		return;
+	}
+	// 这个函数的参数为：form，data，作用就是把data放到from
+	var courseGridData = courseGrid.getSelected();
+	Form.loadForm(courseForm, courseGridData);
+	courseWin = $.ligerDialog.open( {
+		width : 400,
+		height : 400,
+		title : "修改课程",
+		target : courseForm,
+		buttons : [ {
+			text : "提交",
+			width : 80,
+			onclick : edit_save
+		}, {
+			text : "取消",
+			width : 80,
+			onclick : edit_cancel
+		} ]
+	});
+}
+function edit_save() {
+
+	if (courseForm.valid()) {
+		var row_data = Form.parseJSON(courseForm);
+		// 发往服务器，返回成功后再修改到表格中
+		$.ajax( {
+			url : '/hnfnuzyw/resources/updateCourse.action',
+			data : row_data,
+			type : 'post',
+			success : function(data) {
+				console.log(data);
+				if (data.success) {
+					courseGrid.update(courseGrid.getSelected(), data.model);
+					$.ligerDialog.tip( {
+						title : '提示信息',
+						content : data.message
+					});
+					courseWin.close();
+				} else {
+					$.ligerDialog.error(data.message);
+				}
+			}
+		});
+	}
+}
+function edit_cancel() {
+	courseWin.close();
+}
+// 初始化表单，生成form标签
+function formInit() {
+	var groupicon = "../../../App/Lib/ligerUI/skins/icons/communication.gif";
+	courseForm = $('<form></form>');
+
+	$.ajax( {
+		url : '/hnfnuzyw/resources/listGradesAndSubjects.action',
+		type : 'post',
+		async : false,
+		success : function(data) {
+			courseForm.ligerForm( {
+				inputWidth : 200,
+				labelWidth : 90,
+				space : 40,
+				fields : [ {
+					name : "id",
+					type : "hidden"
+				}, {
+					display : "课程名字",
+					name : "name",
+					newline : true,
+					type : "text",
+					group : "必填信息",
+					groupicon : groupicon,
+					validate : {
+						required : true,
+						maxlength : 30
+					}
+
+				}, {
+					display : "所属年级",
+					name : "gradeId",
+					type : "select",
+					comboboxName : "gradeId",
+					options : {
+						textField : "name",
+						valueField : "id",
+						hideOnLoseFocus : true,
+						valueFieldID : "gradeId",
+						data : data.gradeList
+					}
+				}, {
+					// todo 这个最后要搞成下拉框的形式，从后台取数据
+					display : "所属学科",
+					name : "subjectId",
+					type : "select",
+					comboboxName : "subjectId",
+					options : {
+						textField : "name",
+						valueField : "id",
+						hideOnLoseFocus : true,
+						valueFieldID : "id",
+						data : data.subjectList
+					}
+				} ]
+			}, {
+				name : 'remark',
+				display : '备注',
+				type : 'text',
+				space : 30,
+				labelWidth : 100,
+				newline : true,
+				width : 200
+			});
+			$.metadata.setType("attr", "validate");
+			courseForm.validate( {
+				debug : true,
+				onkeyup : false,
+				errorPlacement : function(error) {
+					$.ligerDialog.error(error[0].innerHTML);
+				}
+			});
+		}
+	});
+}
+
+// 页面加载完成后就开始调用
+$(function() {
+
+	var toolbarItems = [ {
+		text : '增加',
+		click : add_course,
+		icon : 'add',
+		key : 'add'
+	}, {
+		line : true
+	}, {
+		text : '删除',
+		click : delete_course,
+		icon : 'delete',
+		key : 'delete'
+	}, {
+		line : true
+	}, {
+		text : '修改',
+		click : edit_course,
+		icon : 'modify',
+		key : 'modify'
+	}, {
+		line : true
+	}, {
+		text : '刷新',
+		click : update_course,
+		icon : 'refresh',
+		key : 'refresh'
+	} ];
+
+	// todo 以后这个ajaxToolbar要通过ajax取过来
+	var ajaxToolbar = [ {
+		name : 'add'
+	}, {
+		name : 'modify'
+	}, {
+		name : 'delete'
+	}, {
+		name : 'refresh'
+	} ];
+	toolbarItems = Toolbar.confirmToolbar(toolbarItems, ajaxToolbar);
+
+	$.ajax( {
+		url : '/hnfnuzyw/resources/listCourse.action',
+		type : 'post',
+		success : function(data) {
+			courseGrid = $("#courseGrid").ligerGrid( {
+				columns : [ {
+					display : '课程名字',
+					name : 'name',
+					align : 'left',
+					minWidth : 80
+				}, {
+					display : '所属年级',
+					name : 'gradeId',
+					align : 'left',
+					minWidth : 100
+				}, {
+					display : '所属学科',
+					name : 'subjectId',
+					align : 'left',
+					minWidth : 100
+				} ],
+				data : data.courseList,
+				height : '98%',
+				width : '100%',
+				toolbar : {
+					items : toolbarItems
+				}
+			});
+		}
+	});
+	$("#pageloading").hide();
+});
---
+++ hnfnuzyw/src/com/hnfnu/zyw/action/resources/CourseAction.java
@@ -0,0 +1,184 @@
+package com.hnfnu.zyw.action.resources;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.Namespace;
+import org.apache.struts2.convention.annotation.ParentPackage;
+import org.apache.struts2.convention.annotation.Result;
+import org.apache.struts2.convention.annotation.Results;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+
+import com.hnfnu.zyw.dto.resources.CourseDto;
+import com.hnfnu.zyw.dto.resources.GradeDto;
+import com.hnfnu.zyw.dto.resources.SubjectDto;
+import com.hnfnu.zyw.service.resources.ICourseService;
+import com.hnfnu.zyw.service.resources.IGradeService;
+import com.hnfnu.zyw.service.resources.ISubjectService;
+import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.ModelDriven;
+
+@Controller("courseAction")
+@Scope("prototype")
+@ParentPackage("json-default")
+@Results( { @Result(name = "success", type = "json", params = { "root",
+		"action" }) })
+@Namespace("/resources")
+public class CourseAction extends ActionSupport implements
+		ModelDriven<CourseDto> {
+
+	private static final long serialVersionUID = -7199971221300636848L;
+	private CourseDto course = new CourseDto();// 获取页面提交参数
+	private List<GradeDto> gradeList;
+	private List<SubjectDto> subjectList;
+	private boolean success;
+	private String message;
+	private Map<String, Object> courseList;
+
+	@Autowired
+	@Qualifier("courseService")
+	private ICourseService courseService;
+
+	@Autowired
+	@Qualifier("gradeService")
+	private IGradeService gradeService;
+
+	@Autowired
+	@Qualifier("subjectService")
+	private ISubjectService subjectService;
+
+	// 添加课程
+	@Action(value = "addCourse")
+	public String add() {
+		success = courseService.add(course);
+		if (success) {
+			message = "添加课程成功！";
+		} else {
+			message = "添加课程失败！";
+		}
+		return SUCCESS;
+	}
+
+	// 修改课程
+	@Action(value = "updateCourse")
+	public String update() {
+		success = courseService.update(course);
+		if (success) {
+			message = "修改课程成功！";
+		} else {
+			message = "修改课程失败！";
+		}
+		return SUCCESS;
+	}
+
+	/**
+	 * 根据课程ID查询一个课程
+	 * 
+	 * @return
+	 */
+	@Action(value = "loadCourse")
+	public String load() {
+		course = courseService.load(course.getId());
+		return SUCCESS;
+	}
+
+	/**
+	 * 根据课程id删除一个课程
+	 * 
+	 * @return
+	 */
+
+	@Action(value = "deleteCourse")
+	public String delete() {
+		success = courseService.delete(course.getId());
+		if (success) {
+			message = "删除课程成功！";
+		} else {
+			message = "删除课程失败！";
+		}
+		return SUCCESS;
+	}
+
+	// 获取表中所有课程，用Map装，为了分页的需要加上Rows和Total
+	@Action(value = "listCourse")
+	public String list() {
+		courseList = courseService.listCourse();
+		return SUCCESS;
+	}
+	
+	@Action(value = "listGradesAndSubjects")
+	public String listGradesAndSubjects() {
+		gradeList = gradeService.list();
+		subjectList = subjectService.list(); 
+		return SUCCESS;
+	}
+
+	/* get set */
+	
+	
+	public ICourseService getCourseService() {
+		return courseService;
+	}
+
+	public List<GradeDto> getGradeList() {
+		return gradeList;
+	}
+
+	public void setGradeList(List<GradeDto> gradeList) {
+		this.gradeList = gradeList;
+	}
+
+	public List<SubjectDto> getSubjectList() {
+		return subjectList;
+	}
+
+	public void setSubjectList(List<SubjectDto> subjectList) {
+		this.subjectList = subjectList;
+	}
+
+	public void setCourseService(ICourseService courseService) {
+		this.courseService = courseService;
+	}
+	
+
+	public IGradeService getGradeService() {
+		return gradeService;
+	}
+
+	public void setGradeService(IGradeService gradeService) {
+		this.gradeService = gradeService;
+	}
+
+	public ISubjectService getSubjectService() {
+		return subjectService;
+	}
+
+	public void setSubjectService(ISubjectService subjectService) {
+		this.subjectService = subjectService;
+	}
+
+	public CourseDto getModel() {
+		return course;
+	}
+
+	public CourseDto getCourse() {
+		return course;
+	}
+
+	public boolean isSuccess() {
+		return success;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public Map<String, Object> getCourseList() {
+		return courseList;
+	}
+
+}
