343df694bb4a1cbb572521125501186b6b1cd2fd
xjyaitf@gmail.com <xjyaitf@gmail.com@8bd0d043-5b41-ff9d-f009-2dea3a7eb2be>
2013-07-13 13:10:31 +0000

    上传资源插件的按钮已经加入了。

--- hnfnuzyw/WebRoot/App/Resources/Source/SourceManage.js
+++ hnfnuzyw/WebRoot/App/Resources/Source/SourceManage.js
@@ -6,7 +6,7 @@ var sourceWin = null;//显示的窗口
 //增加课程
 function add_source(){
     formInit();
-
+    sourceForm.prepend('<input type="file" id="file_upload" name="fileName">');
     sourceWin = $.ligerDialog.open( {
         width : 400,
         height : 550,
@@ -22,6 +22,102 @@ function add_source(){
             onclick : add_cancel
         } ]
     });
+    $("#file_upload").uploadify({
+        debug:false,
+        auto:true,//是否自动上传
+        height: 30,
+        buttonText:'上传资源',
+        button_image_url: "/hnfnuzyw/App/Lib/Uploadify/blank.jsp",
+        cancelImage:'/hnfnuzyw/App/Lib/Uploadify/uploadify-cancel.png',
+        swf : '/hnfnuzyw/App/Lib/Uploadify/uploadify.swf',
+        // expressInstall:'App/Lib/Uploadify/expressInstall.swf',
+        uploader : '/hnfnuzyw/upload/fileUpload.action', //后台处理上传文件的action
+        width : 120 ,
+        multi : false,//是否允许多个文件上传
+        queueID : 'uploadfileQueue',
+        fileObjName : 'fileName', //与后台Action中file属性一样
+        //formData : { },//附带值
+        successTimeout : 99999,//上传超时时间
+        overrideEvents : [ 'onDialogClose' ],
+        // fileTypeDesc : '上传文件支持的文件格式:jpg,jpge,gif,png',
+        // fileTypeExts : '*.*',//*.jpg;*.jpge;*.gif;*.png
+        //queueSizeLimit : 3,//
+        simUploadLimit:1,//一次可以上传1个文件
+        fileSizeLimit : '2048MB',//上传文件最大值
+        //返回一个错误，选择文件的时候触发
+        onSelectError : function(file, errorCode, errorMsg) {
+            switch (errorCode) {
+                case -100:
+                    alert("上传的文件数量已经超出系统限制的"
+                        + $('#file_upload').uploadify(
+                        'settings',
+                        'queueSizeLimit') + "个文件！");
+                    break;
+                case -110:
+                    alert("文件 ["
+                        + file.name
+                        + "] 大小超出系统限制的"
+                        + $('#file_upload')
+                        .uploadify('settings',
+                        'fileSizeLimit')
+                        + "大小！");
+                    break;
+                case -120:
+                    alert("文件 [" + file.name + "] 大小异常！");
+                    break;
+                case -130:
+                    alert("文件 [" + file.name + "] 类型不正确！");
+                    break;
+            }
+        },
+        //每次更新上载的文件的进展
+        onUploadProgress : function(file, bytesUploaded,
+                                    bytesTotal, totalBytesUploaded,
+                                    totalBytesTotal) {
+            //alert(bytesUploaded);
+            //有时候上传进度什么想自己个性化控制，可以利用这个方法
+            //使用方法见官方说明
+        },
+        //检测FLASH失败调用
+        onFallback : function() {
+            alert("您未安装FLASH控件，无法上传图片！请安装FLASH控件后再试。");
+        },
+        //上传到服务器，服务器返回相应信息到data里
+        onUploadSuccess : function(file, data, response) {
+            /*
+            var fileNameAndPath = data.split(",");
+            var phtml = "__tag_143$33_<a href='#' onclick=downLoad('"
+                + fileNameAndPath[1]
+                + "')>"
+                + fileNameAndPath[0]
+                + "__tag_143$112_<img alt='删除' src='js/uploadify/uploadify-cancel.png' onclick=delFile(this)>__tag_143$192_";
+            if ($("#uploadfileQueue p").length == 0) {
+                $("#uploadfileQueue").html(phtml);
+            } else {
+                $("#uploadfileQueue p:last").after(phtml);
+            }
+            alert(data+"上传成功");
+            */
+        },
+        onSelect : function(file) {
+            alert(file.name);
+        },
+        removeCompleted : true,//上传的文件进度条是否消失
+        requeueErrors : false,
+        removeTimeout : 2,//进度条消失的时间，默认为3
+        progressData : "percentage",//显示上传的百分比
+        onUploadError : function(file, errorCode, errorMsg,
+                                 errorString, swfuploadifyQueue) {
+            $("#dialog-message").html(errorString);
+        },
+        onError : function(event, queueID, fileObj) {
+            alert("文件:" + fileObj.name + " 上传失败");
+        }
+    });
+    //删除文件
+    function delFile(obj) {
+        $(obj).parent().remove();
+    }
 }
 // 增加课程的保存按钮事件
 function add_save() {
