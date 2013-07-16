7dadd7c6f9575e80d5da630f69b62d4b9499cd6b
xjyaikj@gmail.com <xjyaikj@gmail.com@8bd0d043-5b41-ff9d-f009-2dea3a7eb2be>
2013-07-16 03:00:55 +0000

    改了下昨天的视图

---
+++ trunk/hnfnuzyw/src/com/hnfnu/zyw/dao/resources/SourceMoreVoDaoImpl.java
@@ -0,0 +1,12 @@
+package com.hnfnu.zyw.dao.resources;
+
+import org.springframework.stereotype.Repository;
+
+import com.hnfnu.zyw.dao.base.BaseDao;
+import com.hnfnu.zyw.vo.SourceMoreVo;
+
+@Repository("sourceMoreVoDao")
+public class SourceMoreVoDaoImpl extends BaseDao<SourceMoreVo> implements
+		ISourceMoreVoDao {
+
+}
