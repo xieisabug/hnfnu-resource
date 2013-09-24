5e703ea4fe7d4bebbd4c7a4e92bc70f5dc5c061c
xjyaitf@gmail.com <xjyaitf@gmail.com@8bd0d043-5b41-ff9d-f009-2dea3a7eb2be>
2013-09-24 13:08:07 +0000

    加入类别的排序功能。

--- trunk/hnfnuzyw/WebRoot/App/Resources/Category/CategoryManage.js
+++ trunk/hnfnuzyw/WebRoot/App/Resources/Category/CategoryManage.js
@@ -1,4 +1,5 @@
 var categoryGrid = null;// 类别表格
+var sortGrid = null;//用于排序的类别表格
 var categoryFrom = null;// 类别表单
 var categoryWin = null;// 类别窗口
 
@@ -6,19 +7,22 @@ var categoryWin = null;// 类别窗口
 function add_catecory() {
     formInit();
     categoryWin = $.ligerDialog.open({
-        width : 400,
-        height : 200,
-        title : '新增类别',
-        target : categoryFrom,
-        buttons : [ {
-            text : '提交',
-            width : 80,
-            onclick : add_save
-        }, {
-            text : '取消',
-            width : 80,
-            onclick : add_cancel
-        } ]
+        width:400,
+        height:200,
+        title:'新增类别',
+        target:categoryFrom,
+        buttons:[
+            {
+                text:'提交',
+                width:80,
+                onclick:add_save
+            },
+            {
+                text:'取消',
+                width:80,
+                onclick:add_cancel
+            }
+        ]
     });
 }
 // 增加类别的保存按钮事件
@@ -27,15 +31,15 @@ function add_save() {
         var row_data = Form.parseJSON(categoryFrom);
         // 发往服务器，返回成功后再添加到表格中
         $.ajax({
-            url : '../../../resources/addCategory.action',
-            data : row_data,
-            type : 'post',
-            success : function(data) {
+            url:'../../../resources/addCategory.action',
+            data:row_data,
+            type:'post',
+            success:function (data) {
                 if (data.success) {
                     categoryGrid.addRow(data.model);
                     $.ligerDialog.tip({
-                        title : '提示信息',
-                        content : data.message
+                        title:'提示信息',
+                        content:data.message
                     });
                     categoryWin.close();
                 } else {
@@ -51,51 +55,53 @@ function add_cancel() {
 }
 // 修改类别的函数
 function edit_category() {
-        formInit();
+    formInit();
     if (!categoryGrid.getSelected()) {
         $.ligerDialog.warn('请选择您要修改的行.');
         return;
     }
     Form.loadForm(categoryFrom, categoryGrid.getSelected());
     categoryWin = $.ligerDialog.open({
-        width : 400,
-        height : 200,
-        title : '编辑类别',
-        target : categoryFrom,
-        buttons : [ {
-            text : '提交',
-            width : 80,
-            onclick : edit_save
-        }, {
-            text : '取消',
-            width : 80,
-            onclick : edit_cancel
-        } ]
+        width:400,
+        height:200,
+        title:'编辑类别',
+        target:categoryFrom,
+        buttons:[
+            {
+                text:'提交',
+                width:80,
+                onclick:edit_save
+            },
+            {
+                text:'取消',
+                width:80,
+                onclick:edit_cancel
+            }
+        ]
     });
 }
 // 修改类别的保存按钮事件
 function edit_save() {
     if (categoryFrom.valid()) {
         var row_data = Form.parseJSON(categoryFrom);
-        // todo 需要发往服务器，返回成功后再修改到表格中
-        $ .ajax({
-                url : '../../../resources/updateCategory.action',
-                data : row_data,
-                type : 'post',
-                success : function(data) {
-                    if (data.success) {
-                        categoryGrid.update(categoryGrid.getSelected(),
-                            data.model);
-                        $.ligerDialog.tip({
-                            title : '提示信息',
-                            content : data.message
-                        });
-                        categoryWin.close();
-                    } else {
-                        $.ligerDialog.error(data.message);
-                    }
+        $.ajax({
+            url:'../../../resources/updateCategory.action',
+            data:row_data,
+            type:'post',
+            success:function (data) {
+                if (data.success) {
+                    categoryGrid.update(categoryGrid.getSelected(),
+                        data.model);
+                    $.ligerDialog.tip({
+                        title:'提示信息',
+                        content:data.message
+                    });
+                    categoryWin.close();
+                } else {
+                    $.ligerDialog.error(data.message);
                 }
-            });
+            }
+        });
     }
 }
 // 修改类别的取消按钮事件
@@ -110,17 +116,17 @@ function delete_category() {
         return;
     }
     var row_data = categoryGrid.getSelected();
-    $.ligerDialog.warn('<p style="color:red;font-weight: bolder;">（警告：删除此类别后，类别下所有的资源会全部删除。）</p>确认删除' + row_data.name + '？', '删除类别', function(r) {
+    $.ligerDialog.warn('<p style="color:red;font-weight: bolder;">（警告：删除此类别后，类别下所有的资源会全部删除。）</p>确认删除' + row_data.name + '？', '删除类别', function (r) {
         if (r) {
             $.ajax({
-                url : '../../../resources/deleteCategory.action',
-                data : row_data,
-                type : 'post',
-                success : function(data) {
+                url:'../../../resources/deleteCategory.action',
+                data:row_data,
+                type:'post',
+                success:function (data) {
                     if (data.success) {
                         $.ligerDialog.tip({
-                            title : '提示信息',
-                            content : data.message
+                            title:'提示信息',
+                            content:data.message
                         });
                         categoryGrid.deleteSelectedRow();
                         categoryWin.close();
@@ -132,74 +138,150 @@ function delete_category() {
         }
     });
 }
+
+//排序类别
+function sort_category() {
+    $.ajax({
+        //todo url是错误的，需要改成跟order有关的
+        url:'../../../resources/listCategory.action',
+        type:'post',
+        success:function (data) {
+            var s = $('#sortGrid');
+            sortGrid = s.ligerGrid({
+                columns:[
+                    {
+                        display:'类别名称',
+                        name:'name',
+                        width:200
+                    }
+                ],
+                width:250,
+                height:400,
+                pageSize:30,
+                data:data.categoryList,
+                rownumbers:true,
+                rowDraggable:true
+            });
+            categoryWin = $.ligerDialog.open({
+                width:300,
+                height:400,
+                title:'类别排序',
+                target:s,
+                buttons:[
+                    {
+                        text:'提交',
+                        width:80,
+                        onclick:sort_save
+                    },
+                    {
+                        text:'取消',
+                        width:80,
+                        onclick:sort_cancel
+                    }
+                ]
+            });
+            //console.log($(".l-grid2",categoryWin.element));
+            $(".l-grid2",categoryWin.element).css({width:223});
+            $("#pageloading").hide();
+        }
+    });
+}
+
+function sort_save(){
+    var objArray = sortGrid.getData();
+    var ids;
+    for(var i = 0; i < objArray.length; i++){
+        ids = objArray[i].id + ';';
+    }
+    //todo 还没提交
+}
+
+function sort_cancel(){
+    categoryWin.close();
+}
+
 // 初始化表单，生成form标签
 function formInit() {
     categoryFrom = $('<form></form>');
     categoryFrom.ligerForm({
-        inputWidth : 280,
-        fields : [ {
-            name : 'id',
-            type : "hidden"
-        }, {
-            display : '类别名称',
-            name : 'name',
-            type : 'text',
-            space : 30,
-            labelWidth : 100,
-            width : 220,
-            newline : true,
-            validate : {
-                required : true,
-                maxlength : 22
+        inputWidth:280,
+        fields:[
+            {
+                name:'id',
+                type:"hidden"
+            },
+            {
+                display:'类别名称',
+                name:'name',
+                type:'text',
+                space:30,
+                labelWidth:100,
+                width:220,
+                newline:true,
+                validate:{
+                    required:true,
+                    maxlength:22
+                }
+            },
+            {
+                display:'备注',
+                name:'remark',
+                type:'text',
+                space:30,
+                labelWidth:100,
+                width:220,
+                newline:true
             }
-        }, {
-            display : '备注',
-            name : 'remark',
-            type : 'text',
-            space : 30,
-            labelWidth : 100,
-            width : 220,
-            newline : true
-        } ]
+        ]
     });
     $.metadata.setType("attr", "validate");
     categoryFrom.validate({
-        debug : true,
-        onkeyup : false,
-        errorPlacement : function(error,element) {
+        debug:true,
+        onkeyup:false,
+        errorPlacement:function (error, element) {
             error.appendTo(element.parent().parent().parent().parent());
         }
     });
 }
 // 初始化表格
-$(function() {
-    var toolbarItems = [ {
-        text : '新增类别',
-        click : add_catecory,
-        icon : 'add',
-        key : 'add'
-    }, {
-        text : '修改类别',
-        click : edit_category,
-        icon : 'modify',
-        key : 'modify'
-    }, {
-        text : '删除类别',
-        click : delete_category,
-        icon : 'delete',
-        key : 'delete'
-    } ];
+$(function () {
+    var toolbarItems = [
+        {
+            text:'新增类别',
+            click:add_catecory,
+            icon:'add',
+            key:'add'
+        },
+        {
+            text:'修改类别',
+            click:edit_category,
+            icon:'modify',
+            key:'modify'
+        },
+        {
+            text:'删除类别',
+            click:delete_category,
+            icon:'delete',
+            key:'delete'
+        },
+        {
+            text:'排序类别',
+            click:sort_category,
+            icon:'load',
+            key:'sort'
+        }
+    ];
     var menuId = window.parent.tab.getSelectedTabItemID();
     $.ajax({
-        url : '../../../system/listFunctionIdList.action',
-        type : 'post',
-        data : {
-            menuId : menuId.substr(0,menuId.indexOf("t"))
+        url:'../../../system/listFunctionIdList.action',
+        type:'post',
+        data:{
+            menuId:menuId.substr(0, menuId.indexOf("t"))
         },
-        success : function(data) {
+        success:function (data) {
             var idList = data.functionIdList.split(";");
             var ajaxToolbar = [];
-            for(var i = 0; i<idList.length; i++){
+            for (var i = 0; i < idList.length; i++) {
                 ajaxToolbar.push({name:parent.hnfnu.functionList[idList[i]]});
             }
             toolbarItems = Toolbar.confirmToolbar(toolbarItems, ajaxToolbar);
@@ -207,28 +289,30 @@ $(function() {
     });
 
     $.ajax({
-        url : '../../../resources/listCategory.action',
-        type : 'post',
-        success : function(data) {
+        url:'../../../resources/listCategory.action',
+        type:'post',
+        success:function (data) {
             categoryGrid = $('#categoryGrid').ligerGrid({
-                columns : [
+                columns:[
                     // { display:'ID', name:'id', align:'left', width:100 },
                     {
-                        display : '类别名称',
-                        name : 'name',
-                        width : 200
-                    }, {
-                        display : '备注',
-                        name : 'remark',
-                        align : 'left',
-                        width : 800
-                    } ],
-                width : '99%',
-                height : '98%',
-                pageSize : 20,
-                data : data.categoryList,
-                toolbar : {
-                    items : toolbarItems
+                        display:'类别名称',
+                        name:'name',
+                        width:200
+                    },
+                    {
+                        display:'备注',
+                        name:'remark',
+                        align:'left',
+                        width:800
+                    }
+                ],
+                width:'99%',
+                height:'98%',
+                pageSize:30,
+                data:data.categoryList,
+                toolbar:{
+                    items:toolbarItems
                 }
             });
             $("#pageloading").hide();
