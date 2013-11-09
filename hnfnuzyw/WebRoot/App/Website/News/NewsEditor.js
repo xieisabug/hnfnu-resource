2fa6423eb7db5a5fdee3c3e67911c0d31e628e41
xjyaikj@gmail.com <xjyaikj@gmail.com@8bd0d043-5b41-ff9d-f009-2dea3a7eb2be>
2013-11-09 06:35:54 +0000

    新闻管理没写完

---
+++ hnfnuzyw/WebRoot/App/Website/News/NewsEditor.js
@@ -0,0 +1,12 @@
+$(function () {
+    var ue = UE.getEditor('editor');
+
+    function getContent() {
+        var arr = [];
+        arr.push("使用editor.getContent()方法可以获得编辑器的内容");
+        arr.push("内容为：");
+        arr.push(UE.getEditor('editor').getContent());
+        alert(arr.join("\n"));
+    }
+
+});
