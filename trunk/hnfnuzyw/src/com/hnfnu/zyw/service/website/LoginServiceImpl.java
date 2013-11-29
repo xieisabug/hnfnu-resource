c84b5885d8ab09f1d23c4328a86ce6cb32d13aae
xjyaitf@gmail.com <xjyaitf@gmail.com@8bd0d043-5b41-ff9d-f009-2dea3a7eb2be>
2013-11-29 11:38:00 +0000

    获取登陆后欢迎界面的图表没有完成。

--- trunk/hnfnuzyw/src/com/hnfnu/zyw/service/website/LoginServiceImpl.java
+++ trunk/hnfnuzyw/src/com/hnfnu/zyw/service/website/LoginServiceImpl.java
@@ -1,17 +1,24 @@
 package com.hnfnu.zyw.service.website;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
+import com.hnfnu.zyw.dao.system.IMenuDao;
 import com.hnfnu.zyw.dao.system.IStudentDao;
 import com.hnfnu.zyw.dao.system.IUserDao;
+import com.hnfnu.zyw.dao.system.IUserRoleMenuVoDao;
 import com.hnfnu.zyw.dao.website.ILoginDao;
+import com.hnfnu.zyw.dto.system.MenuDto;
 import com.hnfnu.zyw.dto.system.StudentDto;
 import com.hnfnu.zyw.dto.system.UserDto;
 import com.hnfnu.zyw.dto.system.ValidateMessege;
+import com.hnfnu.zyw.vo.UserRoleMenuVo;
 
 @Service("loginService")
 public class LoginServiceImpl implements ILoginService {
@@ -22,8 +29,14 @@ public class LoginServiceImpl implements ILoginService {
 	@Qualifier("userDao")
 	public IUserDao userDao;
 	@Autowired
+	@Qualifier("menuDao")
+	public IMenuDao menuDao;
+	@Autowired
 	@Qualifier("studentDao")
 	public IStudentDao studentDao;
+	@Autowired
+	@Qualifier("userRoleMenuVoDao")
+	public IUserRoleMenuVoDao userRoleMenuVoDao;
 
 	public ValidateMessege validateUser(UserDto user) {
 		String hql = "from UserDto where username='" + user.getUsername() + "'";
@@ -65,7 +78,8 @@ public class LoginServiceImpl implements ILoginService {
 	}
 
 	public ValidateMessege validateStudent(StudentDto student) {
-		String hql = "from StudentDto where username='" + student.getUsername() + "'";
+		String hql = "from StudentDto where username='" + student.getUsername()
+				+ "'";
 		StudentDto u = null;
 		ValidateMessege messege = new ValidateMessege();
 		try {
@@ -92,5 +106,24 @@ public class LoginServiceImpl implements ILoginService {
 				return messege;
 			}
 		}
+	}
+
+	public List<Map<String, Object>> welcomeChart(int id) {
+		String hqlUserRoleMenuVo = "from UserRoleMenuVo where userId=" + id;
+		try {
+			List<UserRoleMenuVo> userRoleMenuVos = userRoleMenuVoDao
+					.list(hqlUserRoleMenuVo);
+			List<Integer> menuId = new ArrayList<Integer>();
+			for (UserRoleMenuVo userRoleMenuVo : userRoleMenuVos) {
+				menuId.add(userRoleMenuVo.getMenuId());
+			}
+			List<MenuDto> menus = menuDao.list("from MenuDto");
+			//TODO 先测试好图表的生成，再来完成数据获取与数据打包的算法
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+		return null;
 	}
 }
