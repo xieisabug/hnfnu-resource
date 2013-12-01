5ad5a797255b186f7622d123ff54a1767088ced8
xjyaitf@gmail.com <xjyaitf@gmail.com@8bd0d043-5b41-ff9d-f009-2dea3a7eb2be>
2013-12-01 08:08:24 +0000

    图表开发完成四个，并在欢迎页面显示成功

---
+++ hnfnuzyw/WebRoot/App/welcome.js
@@ -0,0 +1,268 @@
+$(function () {
+
+    var ddddd = [
+        {
+            type:'LineBasic2D',
+            title:'北京2012年平均温度情况',
+            labels:["一月", "二月", "三月", "四月", "五月", "六月", "七月", "八月", "九月", "十月", "十一月", "十二月"],
+            data:[
+                {
+                    name:'北京',
+                    value:[-9, 1, 12, 20, -50, 5, 32, 29, 22, 12, 0, -6],
+                    color:'#1f7e92',
+                    line_width:2
+                }
+            ]
+        },
+        {
+            type:'Pie2D',
+            title:{
+                text:'2012年第3季度中国第三方手机浏览器市场份额',
+                color:'#3e576f'
+            },
+            data:[
+                {name:'UC浏览器', value:40.0, color:'#4572a7'},
+                {name:'QQ浏览器', value:37.1, color:'#aa4643'},
+                {name:'欧朋浏览器', value:13.8, color:'#89a54e'},
+                {name:'百度浏览器', value:1.6, color:'#80699b'},
+                {name:'海豚浏览器', value:1.4, color:'#92a8cd'},
+                {name:'天天浏览器', value:1.2, color:'#db843d'},
+                {name:'其他', value:4.9, color:'#a47d7c'}
+            ]
+        },
+        {
+            type:'Column2D',
+            title:'Hello World\'s Height In Alphabet',
+            start_scale:0, //设置开始刻度为0
+            end_scale:26, //设置结束刻度为26
+            scale_space:4, //设置刻度间距
+            textUnit:" cm",
+            data:[
+                {name:'H', value:7, color:'#a5c2d5'},
+                {name:'E', value:5, color:'#cbab4f'},
+                {name:'L', value:12, color:'#76a871'},
+                {name:'L', value:12, color:'#76a871'},
+                {name:'O', value:15, color:'#a56f8f'},
+                {name:'W', value:13, color:'#c12c44'},
+                {name:'O', value:15, color:'#a56f8f'},
+                {name:'R', value:18, color:'#9f7961'},
+                {name:'L', value:12, color:'#76a871'},
+                {name:'D', value:4, color:'#6f83a5'}
+            ]
+        }
+    ];
+
+    $.getJSON('website/welcomeChart.action', function (data) {
+        if (data.success) {
+            var wrapDiv = $('#chart');
+            var widthChart = wrapDiv.width()/2-50;
+            var heightChart = widthChart/4*3;
+            var datas = data.data;
+            //2D饼图，2D条形图，2D折线图
+            for (var i = 0; i < datas.length; i++) {
+                //取出要绘制的数据
+                var d = datas[i];
+                //生成一个绘制画布，并装入大容器中
+                var render = $('<div id="render' + i + '"></div>');
+                wrapDiv.append(render);
+
+                //如果是折线图
+                if (d.type == "LineBasic2D") {
+                    /**
+                     * 折线图中必须要有的属性：
+                     * data:显示在折线图中用于生成折线的数据
+                     * title:折线图的标题
+                     * labels:折线图的x轴显示的文字
+                     * 折线图中可选的属性（有默认值）：
+                     * start_scale:坐标系的开始值
+                     * end_scale:坐标系的结束值
+                     * scale_space:坐标系的相隔值
+                     * tip:提示标签，鼠标移上去时会显示
+                     * crosshair:坐标的十字线，鼠标移上去时会显示
+                     */
+                    //可选tip属性
+                    var tip = d.tip || {
+                        enable:true,
+                        shadow:true
+                    };
+                    //可选crosshair属性
+                    var crosshair = d.crosshair || {
+                        enable:true,
+                        line_color:'#62bce9'
+                    };
+                    //可选sub_option属性
+                    var sub_option = d.sub_option || {
+                        label:false,
+                        hollow_inside:true,
+                        point_size:8
+                    };
+                    new iChart.LineBasic2D({
+                        render:'render' + i,
+                        data:d.data,
+                        title:d.title,
+                        align:'center',
+                        background_color:d.background_color || '#fff',
+                        width:widthChart,
+                        height:heightChart,
+                        coordinate:{
+                            width:350,
+                            height:200,
+                            grid_color:'#cccccc',
+                            axis:{
+                                color:'#cccccc',
+                                width:[0, 0, 2, 2]
+                            },
+                            grids:{
+                                vertical:{
+                                    way:'share_alike',
+                                    value:5
+                                }
+                            },
+                            scale:[
+                                {
+                                    position:'left',
+                                    start_scale:d.start_scale || 0,
+                                    end_scale:d.end_scale || 10,
+                                    scale_space:d.scale_space || 1,
+                                    scale_size:2,
+                                    label:{color:'#000', fontsize:11},
+                                    scale_color:'#9f9f9f'
+                                },
+                                {
+                                    position:'bottom',
+                                    label:{color:'#000', fontsize:11},
+                                    labels:d.labels
+                                }
+                            ]
+                        },
+                        tip:tip,
+                        crosshair:crosshair,
+                        sub_option:sub_option
+                    }).draw();//绘制折线图
+
+                    //如果是饼图
+                } else if (d.type == 'Pie2D') {
+                    /**
+                     * 饼图中必须要有的属性：
+                     * data:显示在饼图中用于饼块的数据和文字
+                     * title:饼图的标题
+                     * 饼图中可选的属性（有默认值）：
+                     * offsetx:饼图x轴偏移量
+                     * sub_option:饼块的设置
+                     * radius:饼图的半径
+                     * background_color:背景颜色
+                     */
+                    //可选offsetx属性
+                    var offsetx = d.offsetx || 0;
+                    //可选sub_option属性
+                    var sub_option = d.sub_option || {
+                        label:{
+                            background_color:null,
+                            sign:false, //设置禁用label的小图标
+                            padding:'0 4',
+                            border:{
+                                enable:false,
+                                color:'#666666'
+                            },
+                            fontsize:11,
+                            fontweight:600,
+                            color:'#4572a7'
+                        },
+                        border:{
+                            width:2,
+                            color:'#ffffff'
+                        }
+                    };
+                    //可选radius属性
+                    var radius = d.radius || 120;
+                    //可选background_color属性
+                    var background_color = d.background_color || '#fefefe';
+                    new iChart.Pie2D({
+                        render:'render' + i,
+                        data:d.data,
+                        title:d.title,
+                        width:400,
+                        height:300,
+                        sub_option:sub_option,
+                        shadow:true,
+                        shadow_blur:6,
+                        shadow_color:'#aaaaaa',
+                        shadow_offsetx:0,
+                        shadow_offsety:0,
+                        background_color:background_color,
+                        offsetx:offsetx,
+                        offset_angle:-120,
+                        showpercent:true,
+                        tip:{
+                            enable:true
+                        },
+                        decimalsnum:2,
+                        radius:radius
+                    }).draw();//绘制饼图
+
+                    //如果是柱形图
+                } else if (d.type == 'Column2D') {
+
+                    /**
+                     * 柱形图中必须要有的属性：
+                     * data:显示在柱形图中的条形数据
+                     * title:柱形图的标题
+                     * start_scale:坐标轴开始的最小数字,
+                     * end_scale:坐标轴结束的最大数字,
+                     * scale_space:坐标轴的单位间隔,
+                     */
+                    if(d.tip) {
+                        var tip = {
+                            enable:true,
+                            listeners:{
+                                //tip:提示框对象、name:数据名称、value:数据值、text:当前文本、i:数据点的索引
+                                parseText:function(tip,name,value,text,i){
+                                    return d.tip[i];
+                                }
+                            }
+                        };
+                    } else {
+                        var tip = {
+                            enable:false
+                        }
+                    }
+
+                    new iChart.Column2D({
+                        render:'render' + i,
+                        data:d.data,
+                        title:d.title,
+                        width:400,
+                        height:300,
+                        shadow:true,
+                        shadow_color:'#c7c7c7',
+                        tip:tip,
+                        coordinate:{
+                            scale:[
+                                {
+                                    position:'left',
+                                    start_scale:d.start_scale,
+                                    end_scale:d.end_scale,
+                                    scale_space:d.scale_space,
+                                    listeners:{
+                                        parseText:function (t) {
+                                            return {text:t + d.textUnit}
+                                        }
+                                    }
+                                }
+                            ]
+                        }
+                    }).draw();
+
+                }
+                if(i%2 == 0) {
+                    wrapDiv.append('<div style="width:50px;height: 1px; "></div>');
+                } else {
+                    wrapDiv.append('<div style="width: 100%;height: 20px;"></div>')
+                }
+            }
+        } else {
+            $.ligerDialog.error(data.message);
+        }
+    });
+
+});
--- hnfnuzyw/src/com/hnfnu/zyw/service/website/LoginServiceImpl.java
+++ hnfnuzyw/src/com/hnfnu/zyw/service/website/LoginServiceImpl.java
@@ -2,22 +2,31 @@ package com.hnfnu.zyw.service.website;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.poi.poifs.storage.ListManagedBlock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
+import com.hnfnu.zyw.dao.resources.ISourceDao;
 import com.hnfnu.zyw.dao.system.IMenuDao;
 import com.hnfnu.zyw.dao.system.IStudentDao;
 import com.hnfnu.zyw.dao.system.IUserDao;
 import com.hnfnu.zyw.dao.system.IUserRoleMenuVoDao;
+import com.hnfnu.zyw.dao.website.IChartDao;
 import com.hnfnu.zyw.dao.website.ILoginDao;
+import com.hnfnu.zyw.dto.resources.SourceDto;
 import com.hnfnu.zyw.dto.system.MenuDto;
 import com.hnfnu.zyw.dto.system.StudentDto;
 import com.hnfnu.zyw.dto.system.UserDto;
 import com.hnfnu.zyw.dto.system.ValidateMessege;
+import com.hnfnu.zyw.dto.website.chart.CategoryCount;
+import com.hnfnu.zyw.dto.website.chart.TopUserSource;
+import com.hnfnu.zyw.dto.website.chart.UserCount;
+import com.hnfnu.zyw.utils.WebUtils;
 import com.hnfnu.zyw.vo.UserRoleMenuVo;
 
 @Service("loginService")
@@ -37,6 +46,18 @@ public class LoginServiceImpl implements ILoginService {
 	@Autowired
 	@Qualifier("userRoleMenuVoDao")
 	public IUserRoleMenuVoDao userRoleMenuVoDao;
+	@Autowired
+	@Qualifier("sourceDao")
+	public ISourceDao sourceDao;
+	@Autowired
+	@Qualifier("userCountDao")
+	public IChartDao userCountDao;
+	@Autowired
+	@Qualifier("topUserSourceDao")
+	public IChartDao topUserSourceDao;
+	@Autowired
+	@Qualifier("categoryCountDao")
+	public IChartDao categoryCountDao;
 
 	public ValidateMessege validateUser(UserDto user) {
 		String hql = "from UserDto where username='" + user.getUsername() + "'";
@@ -110,6 +131,7 @@ public class LoginServiceImpl implements ILoginService {
 
 	public List<Map<String, Object>> welcomeChart(int id) {
 		String hqlUserRoleMenuVo = "from UserRoleMenuVo where userId=" + id;
+		List<Map<String, Object>> retList = new ArrayList<Map<String, Object>>();
 		try {
 			List<UserRoleMenuVo> userRoleMenuVos = userRoleMenuVoDao
 					.list(hqlUserRoleMenuVo);
@@ -117,13 +139,208 @@ public class LoginServiceImpl implements ILoginService {
 			for (UserRoleMenuVo userRoleMenuVo : userRoleMenuVos) {
 				menuId.add(userRoleMenuVo.getMenuId());
 			}
-			List<MenuDto> menus = menuDao.list("from MenuDto");
-			//TODO 先测试好图表的生成，再来完成数据获取与数据打包的算法
-			
+			List<MenuDto> menus = menuDao
+					.list("from MenuDto where parentId <> -1");
+			Map<String, Integer> menuNameId = mapMenuNameId(menus);
+
+			// TODO 先测试好图表的生成，再来完成数据获取与数据打包的算法
+			for (UserRoleMenuVo userRoleMenuVo : userRoleMenuVos) {
+				if (userRoleMenuVo.getMenuId() == menuNameId.get("用户管理")) {
+					// 获取用户数量，生成折线图；获取上传资源量，生成条形图
+					retList.add(userCountChart());
+					retList.add(topUserSourceChart());
+				} else if (userRoleMenuVo.getMenuId() == menuNameId.get("资源管理")) {
+					// 获取个人资源下载量最高的8个
+					retList.add(topDownloadSourceChart(id));
+				} else if (userRoleMenuVo.getMenuId() == menuNameId.get("类别管理")) {
+					// 类别资源分布数量饼图
+					retList.add(categoryCountChart());
+				} else if (userRoleMenuVo.getMenuId() == menuNameId.get("年级管理")) {
+					// TODO 年级资源分布数量饼图
+				} else if (userRoleMenuVo.getMenuId() == menuNameId.get("学科管理")) {
+					// TODO 学科资源分布数量饼图
+				} else if (userRoleMenuVo.getMenuId() == menuNameId.get("专题管理")) {
+					// TODO 专题访问前5的2D柱形图
+				}
+			}
+
 		} catch (Exception e) {
 			e.printStackTrace();
 			return null;
 		}
-		return null;
+		return retList;
+	}
+
+	// welcomeChart方法内部使用，将所有的菜单映射成为 name:id 形式的map
+	private Map<String, Integer> mapMenuNameId(List<MenuDto> menus) {
+		Map<String, Integer> m = new HashMap<String, Integer>();
+		for (MenuDto menuDto : menus) {
+			m.put(menuDto.getName(), menuDto.getId());
+		}
+		return m;
+	}
+
+	/** 图表算法 **/
+	// welcomeChart方法生成图表算法之一，用于生成用户数量折线图
+	private Map<String, Object> userCountChart() {
+		Map<String, Object> ret = new HashMap<String, Object>();
+		ret.put("type", "LineBasic2D");// 设置类型为折线
+		ret.put("title", "近期用户数量增长图");// 设置图表标题
+
+		List<UserCount> userCounts = null;
+		try {
+			// 获取最近5个月的用户注册数量（有可能不足5个月）
+			userCounts = userCountDao.list("from UserCount");
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+		List<Integer> labels = new ArrayList<Integer>();
+		List<Integer> values = new ArrayList<Integer>();
+		int max = 0, min = 9999;// 用于设置坐标轴的最大最小值
+		for (UserCount uc : userCounts) {
+			labels.add(uc.getMonth());
+			values.add(uc.getNum());
+			if (max < uc.getNum()) {
+				max = uc.getNum();
+			}
+			if (min > uc.getNum()) {
+				min = uc.getNum();
+			}
+		}
+		int space = (max - min) / 10 == 0 ? 1 : (max - min) / 10;// 计算y轴显示数字的间隔
+		ret.put("start_scale", min - 2 * space > 0 ? min - 2 * space : 0);// 计算最低的数字
+		ret.put("end_scale", max + 2 * space);// 计算最高的数字
+		ret.put("scale_space", space);
+
+		ret.put("labels", labels);// 设置折线图x轴
+
+		// 设置数据显示的格式和数据源
+		List<Map<String, Object>> data = new ArrayList<Map<String, Object>>();
+		Map<String, Object> m = new HashMap<String, Object>();
+		m.put("name", "新用户注册量");
+		m.put("value", values);
+		m.put("color", "#1f7a92");
+		m.put("line_width", 3);
+		data.add(m);
+		ret.put("data", data);
+
+		return ret;
+	}
+
+	/** 图表算法 **/
+	// welcomeChart方法生成图表算法之一，用于生成用户上传资源数
+	private Map<String, Object> topUserSourceChart() {
+		Map<String, Object> ret = new HashMap<String, Object>();
+		ret.put("type", "Column2D");// 设置类型为折线
+		ret.put("title", "Top8用户上传资源");// 设置图表标题
+
+		List<TopUserSource> topUserSources = null;
+		try {
+			// 获取最近5个月的用户注册数量（有可能不足5个月）
+			topUserSources = topUserSourceDao.list("from TopUserSource");
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+		int max = 0, min = 9999;
+		List<Map<String, Object>> data = new ArrayList<Map<String, Object>>();
+		for (TopUserSource tus : topUserSources) {
+			Map<String, Object> map = new HashMap<String, Object>();
+			map.put("name", tus.getName());
+			map.put("value", tus.getNum());
+			map.put("color", WebUtils.getRandomHexColor());
+			data.add(map);
+			if (max < tus.getNum()) {
+				max = tus.getNum();
+			}
+			if (min > tus.getNum()) {
+				min = tus.getNum();
+			}
+		}
+		int space = (max - min) / 10 == 0 ? 1 : (max - min) / 10;// 计算y轴显示数字的间隔
+		ret.put("start_scale", min - 2 * space > 0 ? min - 2 * space : 0);// 计算最低的数字
+		ret.put("end_scale", max + 2 * space);// 计算最高的数字
+		ret.put("scale_space", space);
+
+		ret.put("textUnit", "");
+		ret.put("data", data);
+		return ret;
+	}
+
+	/** 图表算法 **/
+	// welcomeChart方法生成图表算法之一，用于生成下载最高的8个个人资源
+	private Map<String, Object> topDownloadSourceChart(int id) {
+		Map<String, Object> ret = new HashMap<String, Object>();
+		ret.put("type", "Column2D");
+		ret.put("title", "下载量前8资源");
+		List<SourceDto> sources = null;
+		try {
+			sources = sourceDao.list("from SourceDto where createUserId = "
+					+ id + " order by useTimes desc ");
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+		int max = 0, min = 9999;
+		List<Map<String, Object>> data = new ArrayList<Map<String, Object>>();
+		List<String> tip = new ArrayList<String>();
+		// System.out.println(sources.size());
+		for (SourceDto s : sources) {
+			if (data.size() < 8) {
+				Map<String, Object> m = new HashMap<String, Object>();
+				tip.add(s.getName());
+				String nameString = s.getName().length() > 3 ? s.getName()
+						.substring(0, 2) + "..." : s.getName();
+				m.put("name", nameString);
+				m.put("value", s.getUseTimes());
+				m.put("color", WebUtils.getRandomHexColor());
+				data.add(m);
+				if (max < s.getUseTimes()) {
+					max = s.getUseTimes();
+				}
+				if (min > s.getUseTimes()) {
+					min = s.getUseTimes();
+				}
+			}
+		}
+		int space = (max - min) / 10 == 0 ? 1 : (max - min) / 10;// 计算y轴显示数字的间隔
+		ret.put("start_scale", min - 2 * space > 0 ? min - 2 * space : 0);// 计算最低的数字
+		ret.put("end_scale", max + 2 * space);// 计算最高的数字
+		ret.put("scale_space", space);
+		ret.put("textUnit", " 次");
+		ret.put("data", data);
+		ret.put("tip", tip);
+		return ret;
+	}
+
+	/** 图表算法 **/
+	// welcomeChart方法生成图表算法之一，用于生成类别资源总数统计饼图
+	private Map<String, Object> categoryCountChart(){
+		Map<String, Object> ret = new HashMap<String, Object>();
+		ret.put("type", "Pie2D");
+		ret.put("title", "类别资源统计");
+		
+		List<CategoryCount> categoryCounts = null;
+		try {
+			categoryCounts = categoryCountDao.list("from CategoryCount");
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+		long sum = 0;//统计总数据个数，用于计算百分比
+		for(CategoryCount cc : categoryCounts) {
+			sum += cc.getNum();
+		}
+		List<Map<String, Object>> data = new ArrayList<Map<String,Object>>();
+		for(CategoryCount cc : categoryCounts) {
+			Map<String, Object> m = new HashMap<String, Object>();
+			m.put("name", cc.getName());
+			m.put("value", (double)cc.getNum()/sum);
+			m.put("color", WebUtils.getRandomHexColor());
+			data.add(m);
+		}
+		ret.put("data", data);
+		return ret;
 	}
 }
