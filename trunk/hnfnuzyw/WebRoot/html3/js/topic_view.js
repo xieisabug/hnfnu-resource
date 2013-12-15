140bf606addf174abfce9fd13df8e9c59955f05b
xjyaikj@gmail.com <xjyaikj@gmail.com@8bd0d043-5b41-ff9d-f009-2dea3a7eb2be>
2013-12-15 03:40:39 +0000

    笨笨帮胖胖提交的代码

--- trunk/hnfnuzyw/WebRoot/html3/js/topic_view.js
+++ trunk/hnfnuzyw/WebRoot/html3/js/topic_view.js
@@ -1,5 +1,5 @@
 window.addEvent('domready',function(){
-    new Select('searchSelect',[
+    var searchSelect = new Select('searchSelect',[
         {
             name:'站内资源',
             value:'站内资源'
@@ -11,6 +11,8 @@ window.addEvent('domready',function(){
     ], {
         height:34
     }).animate().addButtonClass('btn-group-left');
+    var keyWords = new Input($$('#headSearch input')).addClass('input-group-center');
+    var searchButton = new Button($$('button')[1]).addClass('btn-group-right');
     new Tab('subtopic_tab', {
         tabChangeEvent:'mouseover',
         tabTitleCss:'tabTitle subtopic'
@@ -20,11 +22,11 @@ window.addEvent('domready',function(){
             width:120
         };
         if(index == 0){
-            new Button(item,option).addClass('btn-group-left');
+            new Button(item,option).addClass('btn-group-left').addClass('btn-blue-border');
         } else if(index == 3) {
-            new Button(item).addClass('btn-group-right');
+            new Button(item).addClass('btn-group-right').addClass('btn-blue-border');
         } else {
-            new Button(item,option).addClass('btn-group-center');
+            new Button(item,option).addClass('btn-group-center').addClass('btn-blue-border');
         }
 
     });
--- trunk/hnfnuzyw/WebRoot/html3/js/xkui.js
+++ trunk/hnfnuzyw/WebRoot/html3/js/xkui.js
@@ -1,21 +1,21 @@
 var Tab = new Class({
-    initialize: function (id,option) {
+    initialize: function (id, option) {
         var thiz = this;
         this.option = Object.merge({
-            tabTitleCss : 'tabTitle',
-            tabTitleSelectedCss : 'selected',
-            tabChangeEvent : 'click',
-            tabContentCss : 'tabContent',
-            tabContentSelectedCss : 'selected'
+            tabTitleCss: 'tabTitle',
+            tabTitleSelectedCss: 'selected',
+            tabChangeEvent: 'click',
+            tabContentCss: 'tabContent',
+            tabContentSelectedCss: 'selected'
         }, option);
         this.body = $(id);
         this.body.addClass('tab');
-        $$('#'+id+'>ul').addClass('tabTitles');
-        this.tabTitles = $$('#'+id+'>ul>li');
-        this.tabContents = $$('#'+id+'>div');
-        this.tabTitles.each(function(tab, index){
+        $$('#' + id + '>ul').addClass('tabTitles');
+        this.tabTitles = $$('#' + id + '>ul>li');
+        this.tabContents = $$('#' + id + '>div');
+        this.tabTitles.each(function (tab, index) {
             tab.addClass(thiz.option.tabTitleCss);
-            tab.addEvent(thiz.option.tabChangeEvent,function(){
+            tab.addEvent(thiz.option.tabChangeEvent, function () {
                 thiz.tabContents.removeClass(thiz.option.tabContentSelectedCss);
                 thiz.tabContents[index].addClass(thiz.option.tabContentSelectedCss);
                 thiz.tabTitles.removeClass(thiz.option.tabTitleSelectedCss);
@@ -24,24 +24,24 @@ var Tab = new Class({
         });
         this.tabTitles[0].addClass(thiz.option.tabTitleSelectedCss);
         this.tabContents[0].addClass(thiz.option.tabContentSelectedCss);
-        Array.each(this.tabContents, function(content){
+        Array.each(this.tabContents, function (content) {
             content.addClass(thiz.option.tabContentCss);
         });
         return this;
     },
-    setTabTitleCss : function(className){
+    setTabTitleCss: function (className) {
         this.option.tabTitleCss = className;
         return this;
     },
-    setTabTitleSelectedCss : function(className){
+    setTabTitleSelectedCss: function (className) {
         this.option.tabTitleSelectedCss = className;
         return this;
     },
-    setTabContentCss : function(className) {
+    setTabContentCss: function (className) {
         this.option.tabContentCss = className;
         return this;
     },
-    setTabContentSelectedCss : function(className) {
+    setTabContentSelectedCss: function (className) {
         this.option.tabContentSelectedCss = className;
     }
 
@@ -78,16 +78,14 @@ var Select = new Class({
             thiz.body.set('value', items[0].value);
             //生成html
             var bodyPosition = thiz.body.getPosition();
-            console.log(bodyPosition);
             var bodySize = thiz.body.getSize();
-            console.log(bodySize);
             thiz.selectDiv = new Element('div', {
                 styles: {
                     height: items.length * 30 + 40,
                     width: 200,
                     top: bodyPosition.y + bodySize.y,
                     left: bodyPosition.x,
-                    position:'absolute',
+                    position: 'absolute',
                     display: 'none'//初始不显示
                 },
                 'class': 'selectItemDiv',
@@ -95,21 +93,21 @@ var Select = new Class({
             });
             $$('body').grab(thiz.selectDiv);//将选择的面板添加到网页中
             //对按钮绑定点击事件，使面板可以显示和隐藏
-            thiz.clickFunction =  function () {
+            thiz.clickFunction = function () {
                 if (thiz.selectDiv.getStyle('display') == 'none') {
                     thiz.selectDiv.setStyle('display', 'block');
                 } else {
                     thiz.selectDiv.setStyle('display', 'none');
                 }
             };
-            thiz.body.addEvent('click',thiz.clickFunction);
+            thiz.body.addEvent('click', thiz.clickFunction);
             //绑定鼠标事件，当鼠标移开选择面板的时候会隐藏
-            thiz.mouseLeaveFunction = function(){
+            thiz.mouseLeaveFunction = function () {
                 thiz.setStyle('display', 'none');
             };
-            thiz.selectDiv.addEvent('mouseleave',thiz.mouseLeaveFunction);
+            thiz.selectDiv.addEvent('mouseleave', thiz.mouseLeaveFunction);
             //对选择的每个li绑定点击事件
-            Array.each(thiz.selectDiv.getElements('li'), function (item,index) {
+            Array.each(thiz.selectDiv.getElements('li'), function (item, index) {
                 item.addEvent('click', function () {
                     thiz.body.set('text', thiz.itemNames[index]);
                     thiz.body.set('value', thiz.itemValues[index]);
@@ -118,12 +116,12 @@ var Select = new Class({
         } else {
             new Error("下拉列表的名称和值不能为空,必须指定Select的第二个参数")
         }
-        if(Browser.ie6){
-            thiz.dropDownIcon = new Element('span',{html:'▼'});
+        if (Browser.ie6) {
+            thiz.dropDownIcon = new Element('span', {html: '▼'});
         } else {
             thiz.dropDownIcon = new Element('span.caret');
         }
-        thiz.body.grab(thiz.dropDownIcon,'bottom');
+        thiz.body.grab(thiz.dropDownIcon, 'bottom');
         return thiz;
     },
     //增加选择项
@@ -156,48 +154,48 @@ var Select = new Class({
         return thiz;
     },
     //增加按钮的样式
-    addButtonClass:function(className){
+    addButtonClass: function (className) {
         this.body.addClass(className);
         return this;
     },
     //增加选择面板的样式
-    addSelectPanelClass:function(className){
+    addSelectPanelClass: function (className) {
         this.selectDiv.addClass(className);
         return this;
     },
     //移除按钮的样式
-    removeButtonClass:function(className){
+    removeButtonClass: function (className) {
         this.body.removeClass(className);
         return this;
     },
     //移除选择面板的样式
-    removeSelectPanelClass:function(className){
+    removeSelectPanelClass: function (className) {
         this.selectDiv.removeClass(className);
         return this;
     },
     //增加动画效果
-    animate:function(){
+    animate: function () {
         var thiz = this;
         //动画效果设置
-        var bouncingOut = new Fx.Tween(this.selectDiv,{
-            duration:'1000',
-            transition:'bounce:out'
+        var bouncingOut = new Fx.Tween(this.selectDiv, {
+            duration: '1000',
+            transition: 'bounce:out'
         });
         //重置样式
         this.selectDiv.setStyles({
-            height:0,
-            overflow:'hidden',
-            display:'none'
+            height: 0,
+            overflow: 'hidden',
+            display: 'none'
         });
         //移除以前的点击事件，覆盖点击方法，增加新的带有动画的点击方法，并绑定事件
-        this.body.removeEvent('click',this.clickFunction);
+        this.body.removeEvent('click', this.clickFunction);
         this.clickFunction = function () {
             bouncingOut.stop();
-            if (thiz.selectDiv.getStyle('height') != (thiz.itemNames.length * 30 + 40 + 'px') ) {
+            if (thiz.selectDiv.getStyle('height') != (thiz.itemNames.length * 30 + 40 + 'px')) {
                 thiz.selectDiv.setStyle('display', 'block');
                 bouncingOut.start('height', thiz.itemNames.length * 30 + 40);
             } else {
-                bouncingOut.start('height', 0).chain(function(){
+                bouncingOut.start('height', 0).chain(function () {
                     thiz.selectDiv.setStyle('display', 'none');
                 });
             }
@@ -205,34 +203,33 @@ var Select = new Class({
         this.body.addEvent('click', this.clickFunction);
 
         //移除以前的鼠标移开事件，覆盖方法，并且增加带有动画的鼠标移开方法，并绑定事件
-        this.selectDiv.removeEvent('mouseleave',this.mouseLeaveFunction);
+        this.selectDiv.removeEvent('mouseleave', this.mouseLeaveFunction);
         this.mouseLeaveFunction = function () {
             bouncingOut.stop();
-            bouncingOut.start('height', 0).chain(function(){
+            bouncingOut.start('height', 0).chain(function () {
                 thiz.selectDiv.setStyle('display', 'none');
             });
         };
-        this.selectDiv.addEvent('mouseleave',this.mouseLeaveFunction );
+        this.selectDiv.addEvent('mouseleave', this.mouseLeaveFunction);
         return this;
     }
 });
 var Button = new Class({
-    initialize: function (el,option) {
-        if(instanceOf(el ,Array)) {
+    initialize: function (el, option) {
+        if (instanceOf(el, Array)) {
             throw Error("初始化按钮错误，不能传入一个数组对象，如果要创建按钮组，请创建ButtonGroup。");
             return;
         }
         this.body = el;
-        //console.log(this.body);
         this.option = Object.merge({
-            width:74,
-            height:34,
-            onclick:function(){
+            width: 74,
+            height: 34,
+            onclick: function () {
             }
-        },option);
-        if(this.body.tagName == 'BUTTON') {
-            this.body.addEvent('click',this.option.onclick);
-        } else if(this.body.tagName == 'A') {
+        }, option);
+        if (this.body.tagName == 'BUTTON') {
+            this.body.addEvent('click', this.option.onclick);
+        } else if (this.body.tagName == 'A') {
             this.option.height = 'auto';
             this.option.width = 'auto';
             this.body.addClass('abtn');
@@ -242,17 +239,47 @@ var Button = new Class({
         }
         this.body.addClass('btn');
         this.body.setStyles({
-            width:this.option.width,
-            height:this.option.height
+            width: this.option.width,
+            height: this.option.height
         });
         return this;
     },
-    addClass:function(className) {
+    addClass: function (className) {
         this.body.addClass(className);
         return this;
     },
-    removeClass:function(className){
+    removeClass: function (className) {
         this.body.removeClass(className);
         return this;
     }
+});
+var Input = new Class({
+    initialize: function (el, option) {
+        if (instanceOf(el, Array) && el.length != 1) {
+            throw Error("初始化按钮错误，不能传入一个数组对象，如果要创建按钮组，请创建ButtonGroup。");
+            return;
+        }
+        this.body = el;
+        this.option = Object.merge({
+            width: 240,
+            height: 34,
+            className: 'input-text'
+        }, option);
+
+        this.body.addClass(this.option.className);
+        this.body.setStyles({
+            'width': this.option.width,
+            'height': this.option.height
+        });
+        return this;
+    },
+    addClass: function (className) {
+        this.body.addClass(className);
+        return this;
+    },
+    removeClass: function (className) {
+        this.body.removeClass(className);
+        return this;
+    }
+
 });
