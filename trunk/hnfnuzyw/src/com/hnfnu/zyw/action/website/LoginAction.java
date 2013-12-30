da589fbda86f94ada4f591155c1ec0668e3045d9
xjyaitf@gmail.com <xjyaitf@gmail.com@8bd0d043-5b41-ff9d-f009-2dea3a7eb2be>
2013-12-30 07:25:36 +0000

    添加验证码，并且修改登陆需要验证码的方法

--- trunk/hnfnuzyw/src/com/hnfnu/zyw/action/website/LoginAction.java
+++ trunk/hnfnuzyw/src/com/hnfnu/zyw/action/website/LoginAction.java
@@ -1,7 +1,11 @@
 package com.hnfnu.zyw.action.website;
 
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 import org.apache.struts2.ServletActionContext;
 import org.apache.struts2.convention.annotation.Action;
@@ -12,6 +16,7 @@ import org.apache.struts2.convention.annotation.Results;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Scope;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 
 import com.hnfnu.zyw.action.base.AopNoSuchMethodErrorSolveBaseAction;
@@ -20,6 +25,12 @@ import com.hnfnu.zyw.dto.system.UserDto;
 import com.hnfnu.zyw.dto.system.ValidateMessege;
 import com.hnfnu.zyw.service.website.ILoginService;
 import com.opensymphony.xwork2.ActionContext;
+
+import javax.imageio.ImageIO;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @Controller("loginAction")
 @Scope("prototype")
@@ -33,10 +44,51 @@ public class LoginAction extends AopNoSuchMethodErrorSolveBaseAction{
 	private int loginType;
 	private String username;
 	private String password;
+    private String captcha;
 	private boolean success;
 	private String message;
 	private List<Map<String, Object>> data;
 	private Map<String, Object> info;
+
+    /***************************************此处用于验证码生成***********************************************/
+    /**
+     * 验证码图片的宽度。
+     */
+    private int width = 100;
+
+    /**
+     *  验证码图片的高度。
+     */
+    private int height = 30;
+
+
+    /**
+     * 验证码字符个数
+     */
+    private int codeCount = 4;
+
+    /**
+     * xx
+     */
+    private int xx = width / (codeCount + 1);
+
+    /**
+     * 字体高度
+     */
+    private int fontHeight = height - 2;
+
+    /**
+     * codeY
+     */
+    private int codeY = height - 4;
+
+    /**
+     * codeSequence
+     */
+    char[] codeSequence = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+            'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
+            'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+    /**************************************************************************************/
 
 	@Autowired
 	@Qualifier("loginService")
@@ -83,10 +135,17 @@ public class LoginAction extends AopNoSuchMethodErrorSolveBaseAction{
 	// 根据登陆的身份登陆
 	@Action(value = "login")
 	public String login() {
-		if (loginType == STUDENT) {
+        HttpSession session = ServletActionContext.getRequest().getSession();
+        String validateCode = (String)session.getAttribute("validateCode");
+        if(validateCode != null) {
+            if(!captcha.equals(validateCode)) {
+                success = false;
+                message = "对不起，验证码不正确";
+            }
+        }
+        if (loginType == STUDENT) {
 			return loginStudent();
 		}
-
 		if (loginType == USER) {
 			return loginUser();
 		}
@@ -163,33 +222,103 @@ public class LoginAction extends AopNoSuchMethodErrorSolveBaseAction{
 		return SUCCESS;
 	}
 
-	/* get set */
-	public String getUsername() {
-		return username;
-	}
+    @Action(value = "captchaImg", results = {@Result(type = "stream", name="success")})
+    public String captchaImg(){
+        HttpServletRequest req = ServletActionContext.getRequest();
+        HttpServletResponse res = ServletActionContext.getResponse();
+        // 定义图像buffer
+        BufferedImage buffImg = new BufferedImage(width, height,
+                BufferedImage.TYPE_INT_RGB);
+        Graphics2D gd = buffImg.createGraphics();
 
+        // 创建一个随机数生成器类
+        Random random = new Random();
+
+        // 将图像填充为白色
+        gd.setColor(Color.WHITE);
+        gd.fillRect(0, 0, width, height);
+
+        // 创建字体，字体的大小应该根据图片的高度来定。
+        Font font = new Font("Fixedsys", Font.PLAIN, fontHeight);
+        // 设置字体。
+        gd.setFont(font);
+
+        // 画边框。
+        gd.setColor(Color.BLACK);
+        gd.drawRect(0, 0, width - 1, height - 1);
+
+        // 随机产生100条干扰线，使图象中的认证码不易被其它程序探测到。
+        gd.setColor(Color.BLACK);
+        for (int i = 0; i < 100; i++) {
+            int x = random.nextInt(width);
+            int y = random.nextInt(height);
+            int xl = random.nextInt(12);
+            int yl = random.nextInt(12);
+            gd.drawLine(x, y, x + xl, y + yl);
+        }
+
+        // randomCode用于保存随机产生的验证码，以便用户登录后进行验证。
+        StringBuilder randomCode = new StringBuilder();
+        int red = 0, green = 0, blue = 0;
+
+        // 随机产生codeCount数字的验证码。
+        for (int i = 0; i < codeCount; i++) {
+            // 得到随机产生的验证码数字。
+            String strRand = String.valueOf(codeSequence[random.nextInt(36)]);
+            // 产生随机的颜色分量来构造颜色值，这样输出的每位数字的颜色值都将不同。
+            red = random.nextInt(255);
+            green = random.nextInt(255);
+            blue = random.nextInt(255);
+
+            // 用随机产生的颜色将验证码绘制到图像中。
+            gd.setColor(new Color(red, green, blue));
+            gd.drawString(strRand, (i + 1) * xx, codeY);
+
+            // 将产生的四个随机数组合在一起。
+            randomCode.append(strRand);
+        }
+        // 将四位数字的验证码保存到Session中。
+        HttpSession session = req.getSession();
+        session.setAttribute("validateCode", randomCode.toString());
+
+        // 禁止图像缓存。
+        res.setHeader("Pragma", "no-cache");
+        res.setHeader("Cache-Control", "no-cache");
+        res.setDateHeader("Expires", 0);
+
+        res.setContentType("image/jpeg");
+
+        // 将图像输出到Servlet输出流中。
+        ServletOutputStream sos = null;
+        try {
+            sos = res.getOutputStream();
+            ImageIO.write(buffImg, "jpeg", sos);
+            sos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return SUCCESS;
+    }
+
+	/* get set */
 	public void setUsername(String username) {
 		this.username = username;
-	}
-
-	public String getPassword() {
-		return password;
 	}
 
 	public void setPassword(String password) {
 		this.password = password;
 	}
 
-	public boolean isSuccess() {
+    public void setCaptcha(String captcha) {
+        this.captcha = captcha;
+    }
+
+    public boolean isSuccess() {
 		return success;
 	}
 
 	public String getMessage() {
 		return message;
-	}
-
-	public int getLoginType() {
-		return loginType;
 	}
 
 	public void setLoginType(int loginType) {
