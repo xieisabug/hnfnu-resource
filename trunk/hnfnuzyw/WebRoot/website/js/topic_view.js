bf959a7496db55666ac0abad08004dd880f6ed16
xjyaitf@gmail.com <xjyaitf@gmail.com@8bd0d043-5b41-ff9d-f009-2dea3a7eb2be>
2014-01-01 12:53:01 +0000

    专题查看完成

--- trunk/hnfnuzyw/WebRoot/website/js/topic_view.js
+++ trunk/hnfnuzyw/WebRoot/website/js/topic_view.js
@@ -39,6 +39,10 @@ window.addEvent('domready', function () {
         }
 
     });
+    initStyleAndEvent();
+});
+
+function initStyleAndEvent() {
     $$('.topic-resource-content button').each(function (item) {
         new Button(item, {width: 120});
     });
@@ -46,24 +50,20 @@ window.addEvent('domready', function () {
         item.addEvent('mouseover',function () {
             item.getChildren('table .topic-resource-btn').setStyle('display', 'block');
         }).addEvent('mouseleave', function () {
-            item.getChildren('table .topic-resource-btn').setStyle('display', 'none');
-        });
+                item.getChildren('table .topic-resource-btn').setStyle('display', 'none');
+            });
     });
-    /*$('more').addEvent('click',function(){
-        new Request.JSON({
-            url:"../topic/page",
-            onSuccess:function(data){
+}
 
-            }
-        }).get();
-    });*/
-});
 function more(subtitleId){
     var move = $$('.tabContent.selected>.more');
+    var content =  $$('.tabContent.selected')[0];
     var page = move.getProperty("page");
-    /*new Request.JSON({
+    page++;
+    new Request.JSON({
         url:"../topic/page",
         onSuccess:function(data){
+            var oldH = content.getScrollSize().y;
             var topicSources = data.topicSources;
             var joinSources = data.joinSources;
             var html = '',i = 0;
@@ -73,23 +73,64 @@ function more(subtitleId){
             for(i = 0; i<joinSources.length; i++) {
                 html += generateSourceItem(joinSources[i]);
             }
-            move.inject(html,'before');
+            new Element('div',{
+                html:html
+            }).getChildren().each(function(item){
+                    move.grab(item,'before');
+                });
+            initStyleAndEvent();
+            content.setStyle('height',oldH);
+            var h = content.getScrollSize().y;
+            var fx = new Fx.Morph(content, {
+                duration: 1000,
+                transition:Fx.Transitions['Quad']['easeIn']
+            });
+            fx.start({
+                height:[oldH, h]
+            });
             if( (topicSources.length<8 && joinSources.length<8 ) ) {
                 move.destroy();
             }
         }
     }).get({
             subtitleId : subtitleId,
-            page:page+1
-        });*/
-    var html = new Elements();
-    html.append(new Element('div',{
-        html:"<p>我是插入的~~</p>"
-    }));
-    move.grab(html,'before');
-    page++;
+            pageIndex:page
+        });
+
     move.setProperty("page",page);
 }
 function generateSourceItem(data) {
-
+    var html = '';
+    html += '<div class="topic-resource-content">';
+    html += '    <table>';
+    html += '        <tr>';
+    html += '            <td style="width: 140px; text-align: center" rowspan="3">';
+    html += '                <img src="' + basePath + 'website/image/file_icon_'+data.mediaFormat+'.png" style="width:77px; height:77px; display: inline;">';
+    html += '            </td>';
+    html += '            <td style="width: 340px;"><span>资源名</span>：'+data.sourceName+'</td>';
+    if(data.price == 0) {
+        html += '            <td style="width: 200px;"><span>资源币</span>：免费</td>';
+    } else {
+        html += '            <td style="width: 200px;"><span>资源币</span>：'+data.price+'</td>';
+    }
+    html += '            <td style="width: 130px; text-align: center" rowspan="3">';
+    html += '                <div class="topic-resource-btn">';
+    if(onlineViewFormat.contains(data.mediaFormat+',')) {
+        html += '                    <button value="在线预览" onclick="online/view?topicSourceid='+ data.id +'">在线预览</button>';
+    }
+    html += '                    <button value="下载资源" onclick="">下载资源</button>';
+    html += '                </div>';
+    html += '            </td>';
+    html += '        </tr>';
+    html += '        <tr>';
+    html += '            <td><span>媒体类型</span>：'+data.mediaType+'</td>';
+    html += '            <td><span>文件大小</span>：'+data.fileSize+'M</td>';
+    html += '        </tr>';
+    html += '        <tr>';
+    html += '            <td><span>出版社</span>：'+data.publisher+'</td>';
+    html += '            <td>&nbsp;</td>';
+    html += '        </tr>';
+    html += '    </table>';
+    html += '</div>';
+    return html;
 }
