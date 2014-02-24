be4344f1e97e122c6025cba22bb15c0057a7aaa3
xjyaitf@gmail.com <xjyaitf@gmail.com@8bd0d043-5b41-ff9d-f009-2dea3a7eb2be>
2014-02-24 03:21:59 +0000

    添加一个调试输出的方法，能够判断是否支持console.log，避免IE等浏览器报错。（未支持IE输出，需要继续完成）

--- hnfnuzyw/WebRoot/website/js/xkui.js
+++ hnfnuzyw/WebRoot/website/js/xkui.js
@@ -1384,3 +1384,10 @@ Date.prototype.Format = function (fmt) { //author: meizz
         if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
     return fmt;
 }
+Log = {
+    i : function(o){
+        if(console && console.log) {
+            console.log(o);
+        }
+    }
+}
