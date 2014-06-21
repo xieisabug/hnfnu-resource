013835a87f0a9d9f2eb8ff8456bb4d493911071a
xjyaikj@gmail.com <xjyaikj@gmail.com@8bd0d043-5b41-ff9d-f009-2dea3a7eb2be>
2014-06-21 03:17:13 +0000

    服务器部署URl的java和class

---
+++ design/服务器部署Url.java
@@ -0,0 +1,12 @@
+package com.hnfnu.zyw.utils;
+
+public class Url {
+	//realPath是文件上传的真正的路径
+	public static final String realPath ="D:\\Tomcat 7.0\\webapps\\Root\\" ;
+	//这个是登陆界面的相对路径，
+	public static final String LOGINPATH = "/login.html";
+	//删除新闻的时候要删除新闻的附件，这个是附件的路径
+	public static final String UEDITOR_FILE = "D:\\Tomcat 7.0\\webapps\\Root\\ueditor\\js\\upload\\";
+	//freemark要用到的生成路径
+    public final static String FREEMRAK_FILE_PATH = "D:\\Tomcat 7.0\\webapps\\Root\\";
+}
